78bd1a0cff2268887e09e2cb603082287f470b78
DangKhoaNguyen <ngudy131@mymail.unisa.edu.au>
2025-03-15 19:31:09 +1100

    Update CNN modle,aws_login,app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import mysql.connector
+import CNN_Model
 import os
 
 app = Flask(__name__)
@@ -10,13 +11,13 @@ load_dotenv(dotenv_path='aws_login.env')
 
 
 HOST = os.getenv('HOST', 'aws_login')
-USERNAME = os.getenv('USERNAME', 'aws_login')
+USERNAME_DB = os.getenv('USERNAME_DB', 'aws_login')
 PASSWORD = os.getenv('PASSWORD', 'aws_login')
 DATABASE = os.getenv('DATABASE', 'aws_login')
 
 db = mysql.connector.connect(
         host= HOST,
-        user= USERNAME,
+        user= USERNAME_DB,
         password= PASSWORD,
         database= DATABASE
     )

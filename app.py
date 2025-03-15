727a5099cee7057f41429db5735429effc6962c0
DangKhoaNguyen <ngudy131@mymail.unisa.edu.au>
2025-03-16 05:56:05 +1030

    Updates app.py (Select id)

--- app.py
+++ app.py
@@ -126,6 +126,12 @@ def update():
 @app.route("/missing_paw_results", methods=["POST"])
 def missing_paw_result():
     email = request.form.get("email")
+    
+    query = F"""
+    SELECT id FROM pets
+    WHERE email = {email} 
+    """
+    id = db.excute(query)
     return render_template("missing-paw-results.html", email=email)
 
 def calculate_similarity(found_img_path):
